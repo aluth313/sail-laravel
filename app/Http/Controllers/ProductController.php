7cd601714ef5f4fa247e1ac55c0f7943715241bf
aluth313 <ahmadluthfi@deptechdigital.com>
2023-11-05 15:19:08 +0700

    create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -27,7 +28,9 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request['profit'] = $request->selling_price - $request->purchase_price;
+        $product = Product::create($request->all());
+        return redirect('products');
     }
 
     /**
